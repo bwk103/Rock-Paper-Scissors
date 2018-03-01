3fa57e5a343a7405a6c1be7dcc7845de58f18de0
Ben Kielty <bwk103@gmail.com>
2018-03-01 11:33:55 +0000

    Amend Web Interface
    Changed web interface to allow for a two player game.  If no name
    is provided for player2, the game will be single player - otherwise two
    players fight it out.

--- logic.js
+++ logic.js
@@ -1,18 +1,40 @@
+"use strict";
+
 $(document).ready(function(){
   var game;
 
   $('#play').on('click', function(e){
     e.preventDefault()
-    var player1 = getPlayer($('#player1').val())
-    game = new Game(player1)
+    startGame()
   })
 
   $('button.weapon').on('click', function(e){
     e.preventDefault();
-    var weapon = makeWeapon($(this).attr('data-weapon'))
-    game._player1.setWeapon(weapon)
-    endGame()
+    takeTurn()
   })
+
+  $('#replay').on('click', function(){
+    startGame()
+    $('#result-message').text('')
+    $('#replay').toggleClass('hide')
+  })
+
+  function startGame(){
+    var player1 = getPlayer($('#player1').val())
+    if (isPlayer2()){
+      var player2 = getPlayer($('#player2').val())
+      game = new Game(player1, player2)
+      console.log(game)
+    } else {
+      game = new Game(player1)
+    }
+  }
+
+  function takeTrun(){
+    var weapon = makeWeapon($(this).attr('data-weapon'))
+    game._currentTurn.setWeapon(weapon)
+    isGameOver()
+  }
 
   function endGame(){
     game.setResult()
@@ -20,13 +42,20 @@ $(document).ready(function(){
       $('#replay').toggleClass('hide')
   }
 
-  $('#replay').on('click', function(){
-    game = new Game(getPlayer($('#player1').val()))
-    $('#result-message').text('')
-    $('#replay').toggleClass('hide')
-  })
+  function isGameOver(){
+    if (game._player1._weapon !== undefined && game._player2._weapon !== undefined){
+      endGame()
+    }
+    else {
+      game.switchTurn();
+    }
+  }
 
 })
+
+function isPlayer2(){
+  return $('#player2').val() !== ''
+}
 
 function getPlayer(name){
   return new Player(name)
